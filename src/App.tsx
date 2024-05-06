cc7804257d6ab43747d0c3c611adc3b3b5dea5eb
Yuri Serafim <yuridefranca@gmail.com>
2024-05-06 12:33:41 +0100

    feat: :sparkles: fetch and display 5 todos from the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
+
+type Todo = {
+	id: number;
+	todo: string;
+	completed: boolean;
+	userId: number;
+};
+
+const fetchTodos = async (): Promise<{ todos: Todo[] }> => {
+	const response = await fetch('https://dummyjson.com/todos?limit=5');
+	const data = await response.json();
+	return data;
+};
+
 export default function App() {
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const fetchTodosHandler = async () => {
+		const data = await fetchTodos();
+		setTodos(data.todos);
+	};
+
+	useEffect(() => {
+		fetchTodosHandler();
+	}, []);
+
 	return (
-		<>
-			<h1>Duelbits - Live Coding Interview</h1>
-			<p>Good luck!</p>
-		</>
+		<main>
+			{todos.length > 0 && (
+				<ul>
+					{todos.map((todo) => (
+						<li key={todo.id}>{todo.todo}</li>
+					))}
+				</ul>
+			)}
+		</main>
 	);
 }
